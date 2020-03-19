5f5dc6c529457e5cd8d21ea6cd38896edeb4bf6c
Ginkgodango <merrill.k.nguyen@gmail.com>
2020-03-19 17:11:13 +1100

    MSCI loop completed

--- main_msci.py
+++ main_msci.py
@@ -5,9 +5,9 @@ import pandas as pd
 
 ftp = FTP("ftp.msci.com")
 ftp.login('wnvyzpng', 'hxcksyyx')
-print(ftp.getwelcome())
-print(ftp.pwd())
-print(ftp.dir())
+# print(ftp.getwelcome())
+# print(ftp.pwd())
+# print(ftp.dir())
 
 
 def get_total_size(ftp_dir):
@@ -42,8 +42,50 @@ def load_msci(filepath):
             df = df.append(child1.attrib, ignore_index=True)
     return df
 
-
 # df_msci = load_msci('U:/CIO/#Research/MSCI/20200113core_dm_daily_d/20200113_20200113_CORE_DM_ALL_SECURITY_MAIN_DAILY_D.xml')
+
+def download_msci(ftp_dir):
+    size = 0
+    count = 0
+    parent_dir = ftp.pwd() # get the current directory
+    for filename in ftp.nlst(ftp_dir):
+        # (don't forget to import os)
+        path = os.path.join(parent_dir, filename) # keeps recursively track of the path
+        try:
+            ftp.cwd(path)
+            size += get_total_size(path)
+            ftp.cwd(parent_dir)
+
+        except:
+            ftp.voidcmd('TYPE I')
+            size += ftp.size(path)
+
+        try:
+            ftp.retrbinary("RETR " + filename, open('E:' + str(filename), 'wb').write)
+            with open('E:/download/Record.txt', 'a') as tf:
+                tf.write(str(count) + ',' + str(ftp.size(path)) + ',' + str(path) + ',' + 'Success\n')
+        except:
+            with open('E:/download/Record.txt', 'a') as tf:
+                tf.write(str(count) + ',' + str(ftp.size(path)) + ',' + str(path) + ',' + 'Failed\n')
+
+        count += 1
+        print('Count: ', count, 'Size:', size, 'Filename: ', filename)
+
+        if count == 5:
+            break
+    return size
+
+
+with open('E:/download/Record.txt', 'w') as file:
+    file.write('Count, Size, Filepath, Outcome\n')
+
+download_start = download_msci(ftp.pwd())
+
+
+
+# ftp.retrbinary("RETR " + filename ,open(A, 'wb').write)
+# with open('D:/somefile.txt', 'a') as file:
+#     file.write('Hello\n')
 
 """
 lgs_msci_dict = {
