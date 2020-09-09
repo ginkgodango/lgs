644681cb77d5be0a0ce6e5d715a451d2d2f034d8
Merrill <merrill.k.nguyen@gmail.com>
2020-09-09 22:55:14 +1000

    functional

--- main_functional_msci.py
+++ main_functional_msci.py
@@ -1,15 +1,26 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
+import zipfile
 
 
 def load_msci(filepath):
+    if filepath[-3:] == 'xml':
+        return parse_msci_file(filepath)
+    elif filepath[-3:] == 'zip':
+        return parse_msci_file(open_file_from_zip(filepath))
+    else:
+        print('Could not load:', filepath)
+
+
+def parse_msci_file(filepath):
     return pd.DataFrame([child1.attrib for child0 in ET.parse(filepath).getroot() for child1 in child0])
 
 
+def open_file_from_zip(filepath):
+    return zipfile.ZipFile(filepath, 'r').open(filepath.split('/')[-1].upper()[:-3] + 'xml')
+
+
 if __name__ == "__main__":
-    filepath_1 = r'C:\Users\mnguyen\LGSS\Investments Team - SandPits - SandPits\data\input\test\xml\20200203_20200228_core_ap_sec_main_daily_rif\20200203_20200228_CORE_AP_SEC_MAIN_DAILY_RIF.xml'.replace(
+    filepath_1 = r'C:\Users\mnguyen\LGSS\Investments Team - SandPits - SandPits\data\input\test\xml\20200203_20200228_core_ap_index_main_monthly.zip'.replace(
         "\\", "/")
     df = load_msci(filepath_1)
-    df.to_csv(r'C:\Users\mnguyen\LGSS\Investments Team - SandPits - SandPits\data\input\test\xml\20200203_20200228_core_ap_sec_main_daily_rif\20200203_20200228_CORE_AP_SEC_MAIN_DAILY_RIF.csv'.replace(
-        "\\", "/"), index=False)
-
