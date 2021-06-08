735b3e1652c80bcd6840139d17ae68722f9778ff
Merrill <merrill.k.nguyen@gmail.com>
2021-06-08 16:12:56 +1000

    Correlation matrix

--- AssetClassReview/CorrelationTable.py
+++ AssetClassReview/CorrelationTable.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -32,10 +33,28 @@ def jpm_wide_to_long(df, set_date_name, set_index_name, set_values_name):
     )
 
 
+def asset_class_heatmap(df, period):
+
+    df_period = df[-period:]
+
+    mask = np.triu(df_period.corr())
+
+    plt.figure(figsize=(12.8, 12.8))
+
+    return sns.heatmap(
+        df_period.corr(),
+        annot=True,
+        mask=mask,
+        cmap='coolwarm',
+        square=True,
+        linewidths=3,
+        cbar_kws={"shrink": .5}
+    )
+
+
 if __name__ == "__main__":
     folder_path = "C:/Users/mnguyen/LGSS/Investments Team - SandPits - SandPits/data/input/vendors/jpm/markets/performance/2021/04/"
-    lgs_dictionary_path = 'U:/CIO/#Data/input/lgs/dictionary/2020/12/New Dictionary_v17.xlsx'
-    lgs_allocations_path = 'U:/CIO/#Data/input/lgs/allocations/asset_allocations_2021-02-28.csv'
+    lgs_dictionary_path = 'C:/Users/mnguyen/LGSS/Investments Team - SandPits - SandPits/data/input/lgs/dictionary/2021/03/New Dictionary_v21.xlsx'
     jpm_main_mv_path = folder_path + 'Historical Time Series - Monthly - Main Market Values.xlsx'
     jpm_alts_mv_path = folder_path + 'Historical Time Series - Monthly - Alts Market Values.xlsx'
     jpm_main_returns_path = folder_path + 'Historical Time Series - Monthly - Main Returns.xlsx'
@@ -138,10 +157,15 @@ if __name__ == "__main__":
 
     df_combined_1 = pd.merge(left=df_lgs_dictionary, right=df_jpm, on=['JPM Account Id'], how='inner').sort_values(['LGS Asset Class Level 1', 'JPM Account Id', 'Date'])
 
-    df_AR = df_combined_1[df_combined_1['LGS Asset Class Level 1'].isin(['AR'])]
+    df_combined_2 = df_combined_1[df_combined_1['LGS Open'].isin([1]) & df_combined_1['LGS Liquidity'].isin([0])]
 
-    df_AR_corr = df_AR[['LGS Name', 'Date', 'R_a_m_p']].pivot_table(index='Date', columns='LGS Name', values='R_a_m_p')
+    groupby_dict = dict(list(df_combined_2.groupby(['LGS Asset Class Level 1'])))
 
-    corr_matrix_36_month = df_AR_corr[-36:].corr()
+    for asset_class, df_temp in groupby_dict.items():
 
-    sns.heatmap(corr_matrix_36_month)
+        df_temp = df_temp[['LGS Name', 'Date', 'R_a_m_p']].pivot_table(index='Date', columns='LGS Name', values='R_a_m_p')
+
+        plot = asset_class_heatmap(df_temp, 36)
+
+        plot.figure.savefig("C:/Users/mnguyen/LGSS/Investments Team - SandPits - SandPits/data/output/lgs/correlations/" + str(asset_class) + '.png', dpi=300)
+
